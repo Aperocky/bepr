9cf0826a67cef9ba5f659418211853535d92bd40
Rocky Li <aperocky@gmail.com>
2026-06-08 08:37:38 -0400

    Add human readable timestamps

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,42 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn log(msg: impl std::fmt::Display) {
-    let ts = SystemTime::now()
+    let secs = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap_or_default()
         .as_secs();
-    eprintln!("[{ts}] {msg}");
+    eprintln!("[{}] {msg}", format_utc(secs));
+}
+
+
+fn format_utc(secs: u64) -> String {
+    let (mut days, time) = (secs / 86400, secs % 86400);
+    let (h, m, s) = (time / 3600, (time % 3600) / 60, time % 60);
+
+    let mut year = 1970u64;
+    loop {
+        let days_in_year = if is_leap(year) { 366 } else { 365 };
+        if days < days_in_year {
+            break;
+        }
+        days -= days_in_year;
+        year += 1;
+    }
+
+    let month_days: [u64; 12] = [31, if is_leap(year) { 29 } else { 28 }, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let mut month = 1u64;
+    for &md in &month_days {
+        if days < md {
+            break;
+        }
+        days -= md;
+        month += 1;
+    }
+    let day = days + 1;
+
+    format!("{year}-{month:02}-{day:02}T{h:02}:{m:02}:{s:02}Z")
+}
+
+fn is_leap(y: u64) -> bool {
+    (y % 4 == 0 && y % 100 != 0) || y % 400 == 0
 }
